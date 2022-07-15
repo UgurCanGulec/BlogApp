4ed497fbc86dbe63aec27b538dcd886fe9183a5c
UgurCanGulec <gulecugurcan@gmail.com>
2022-07-15 15:29:57 +0300

    initialize commit

---
+++ src/test/java/com/gulecugurcan/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulecugurcan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
